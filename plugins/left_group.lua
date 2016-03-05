ff28e9edc42eda05c43cfc18bc97246e3bfa4cc1
BHH-team <amirsalib2@yahoo.com>
2016-03-05 17:00:16 +0330

    Create left_group.lua

--- plugins/left_group.lua
+++ plugins/left_group.lua
@@ -14,13 +14,12 @@ end
  
 return {
   patterns = {
-    "^[!/](leavenod32)$",
+    "^[!/](leaveBHHbot)$",
     "^!!tgservice (.+)$",
   },
   run = run
 }
 end
---Copyright and edit; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright and edit; @Xx_minister_salib_xX
+--Persian Translate; @Xx_minister_salib_xX
+--ch : @Xx_etehad_salib_xX
