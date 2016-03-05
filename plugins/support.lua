cd61f13c90ac6402ee6dca2f74a87fc09b3d7420
BHH-team <amirsalib2@yahoo.com>
2016-03-05 16:53:03 +0330

    Create support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -6,7 +6,7 @@ local function callback(extra, success, result)
 end
 
 local function run(msg, matches)
-  local user = 158990680
+  local user = 167268835
 
   if matches[1] == "support" then
     user = 'user#id'..user
@@ -32,7 +32,6 @@ return {
 }
 
 end
---Copyright; @behroozyaghi
---Persian Translate; @behroozyaghi
---ch : @nod32team
---کپی بدون ذکر منبع حرام است
+--Copyright; @Xx_minister_salib_xX
+--Persian Translate; @Xx_minister_salib_xX
+--ch : @Xx_etehad_salib_xX
